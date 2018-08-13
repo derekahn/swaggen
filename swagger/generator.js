90645aa9f0cd99019257c7b3f20f26459b3c7c4a
Derek Ahn <git.derek@gmail.com>
2018-08-13 13:07:14 -0700

    First pass at a swagger/generator

---
+++ swagger/generator.js
@@ -0,0 +1,53 @@
+const { promisify } = require('util');
+
+const mkdirp = promisify(require('mkdirp'));
+const write = promisify(require('fs').writeFile);
+
+const { doc, entry } = require('./boilerplate');
+
+const target = process.argv
+  .map(arg => {
+    if (arg.includes('route')) {
+      return arg
+        .split('=')
+        .pop()
+        .trim();
+    }
+  })
+  .pop();
+
+if (!target) {
+  const err = new Error('swaggen: route=<path> required!');
+  throw err;
+}
+
+(async () => {
+  try {
+    const basePath = `${process.cwd()}/routes/${target}`;
+    const docsDir = `${basePath}/docs`;
+
+    await mkdirp(docsDir);
+
+    const files = require('require-all')({
+      dirname: basePath,
+      filter(fileName) {
+        if (fileName.includes('index')) {
+          return false;
+        }
+        return fileName;
+      },
+      recursive: true,
+    });
+
+    // Write doc files
+    Object.keys(files)
+      .map(name => `${name.split('.js')[0]}.doc.js`)
+      .forEach(async file => {
+        await write(`${docsDir}/${file}`, doc);
+      });
+
+    await write(`${docsDir}/_index.js`, entry(target));
+  } catch (err) {
+    throw err;
+  }
+})();
