5354060f3e1f730ed69289d1f9ae92824b9f565d
Derek Ahn <git.derek@gmail.com>
2018-08-22 10:15:38 -0700

    Configure paths required in example/routes/swagger
    - Due to how I nested the example/ app in a sub directory we have to do special pathing because it’s outside of the scope of example’s package.json.

--- example/routes/swagger/swagger.js
+++ example/routes/swagger/swagger.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const { join } = require('path');
 
+// due to nested swaggen/example/ dir
+const configPath = join(__dirname, '../../../swagger');
+const routesPath = join(__dirname, '../../routes');
+
 const swaggerUI = require('swagger-ui-express');
-const swaggerConfig = require(join(__dirname, '../../../swagger'));
+const swaggerConfig = require(configPath)(routesPath);
 
 router.use('/', swaggerUI.serve, swaggerUI.setup(swaggerConfig));
 
